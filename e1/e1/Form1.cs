89cb61715a3de9c72b089e23729a00147ff9867e
iss2016302580110 <2016302580110@whu.edu.cn>
2017-12-12 09:49:07 +0800

    exercise1

---
+++ e1/e1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace e1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        WebSpider imp = new WebSpider();
+        PracticeInfo pracInfo = new PracticeInfo();
+        List<int> id = new List<int>();
+        List<int> companyId = new List<int>();
+        List<PracticeInfo> info = new List<PracticeInfo>();
+
+        string[] address = { "北京","上海","杭州","成都","深圳",
+                                "西安","苏州","广州","南京","青岛","大连","武汉","百度" };
+
+
+        private void button_download_Click(object sender, EventArgs e)
+        {
+            imp.getIdAndCompanyTd(address, id, companyId);
+            info =imp.getPracticeInfo(id, companyId);
+            pracInfo.setPracticeInfoToFile("..//..//..//data.txt", info);
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_display_Click(object sender, EventArgs e)
+        {
+            displayForm form3 = new displayForm();
+            form3.Show();
+
+        }
+        private void button_renew_Click(object sender, EventArgs e)
+        {
+            RenewForm form4 = new RenewForm();
+            form4.Show();
+
+        }
+
+
+    }
+}
---
+++ e1/e1/Form3.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace e1
+{
+    public partial class displayForm : Form
+    {
+
+        PracticeInfo pracInfo = new PracticeInfo();
+        List<PracticeInfo> practiceInformation = new List<PracticeInfo>();
+        public displayForm()
+        {
+            InitializeComponent();
+
+        }
+
+
+        public void getInformationDisplay(string s)
+        {
+            practiceInformation = pracInfo.getPracticeInfoFromFile(s);
+
+            for (int i = 0; i < practiceInformation.Count; i++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                dataGridView1.Rows.Add(row);
+                dataGridView1.Rows[i].Cells[0].Value = practiceInformation[i].name;
+                dataGridView1.Rows[i].Cells[1].Value = practiceInformation[i].duty;
+                dataGridView1.Rows[i].Cells[2].Value = practiceInformation[i].request;
+                dataGridView1.Rows[i].Cells[3].Value = practiceInformation[i].location;
+                dataGridView1.Rows[i].Cells[4].Value = practiceInformation[i].compname;
+                dataGridView1.Rows[i].Cells[5].Value = practiceInformation[i].compinfo;
+                dataGridView1.Rows[i].Cells[6].Value = practiceInformation[i].compweb;
+
+
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "所有文件(*.*)|*.*";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filepath = fileDialog.FileName;
+                textBox1.AppendText(filepath);
+                getInformationDisplay(filepath);
+            }
+        }
+    }
+}
