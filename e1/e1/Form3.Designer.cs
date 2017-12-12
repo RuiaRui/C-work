8ef9482f32415bb69f55553a62ac34950370ac4e
iss2016302580110 <2016302580110@whu.edu.cn>
2017-12-12 10:02:42 +0800

    ex1

---
+++ e1/e1/Form3.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace e1
+{
+    partial class displayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.duty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.require = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.conName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.conWeb = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.conInfo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "打开：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.duty,
+            this.require,
+            this.address,
+            this.conName,
+            this.conWeb,
+            this.conInfo});
+            this.dataGridView1.Location = new System.Drawing.Point(50, 79);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(813, 407);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // name
+            // 
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.name.DefaultCellStyle = dataGridViewCellStyle1;
+            this.name.HeaderText = "岗位名称";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.name.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.name.Width = 150;
+            // 
+            // duty
+            // 
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.duty.DefaultCellStyle = dataGridViewCellStyle2;
+            this.duty.HeaderText = "岗位职责";
+            this.duty.Name = "duty";
+            this.duty.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.duty.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.duty.Width = 200;
+            // 
+            // require
+            // 
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.require.DefaultCellStyle = dataGridViewCellStyle3;
+            this.require.HeaderText = "岗位要求";
+            this.require.Name = "require";
+            this.require.ReadOnly = true;
+            this.require.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.require.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.require.Width = 200;
+            // 
+            // address
+            // 
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.address.DefaultCellStyle = dataGridViewCellStyle4;
+            this.address.HeaderText = "工作地点";
+            this.address.Name = "address";
+            this.address.ReadOnly = true;
+            this.address.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.address.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // conName
+            // 
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.conName.DefaultCellStyle = dataGridViewCellStyle5;
+            this.conName.HeaderText = "公司名称";
+            this.conName.Name = "conName";
+            this.conName.ReadOnly = true;
+            this.conName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.conName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // conWeb
+            // 
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.conWeb.DefaultCellStyle = dataGridViewCellStyle6;
+            this.conWeb.HeaderText = "公司网址";
+            this.conWeb.Name = "conWeb";
+            this.conWeb.ReadOnly = true;
+            this.conWeb.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.conWeb.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // conInfo
+            // 
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.conInfo.DefaultCellStyle = dataGridViewCellStyle7;
+            this.conInfo.HeaderText = "公司简介";
+            this.conInfo.Name = "conInfo";
+            this.conInfo.ReadOnly = true;
+            this.conInfo.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.conInfo.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.conInfo.Width = 200;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(747, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "打开文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // displayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 568);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "displayForm";
+            this.Text = "display";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn duty;
+        private System.Windows.Forms.DataGridViewTextBoxColumn require;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn conName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn conWeb;
+        private System.Windows.Forms.DataGridViewTextBoxColumn conInfo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ e1/e1/WebSpider.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace e1
+{
+    class WebSpider
+    {
+        //获取网页html
+        string GetContent(string url)
+        {
+            string html = "";
+            // 发送查询请求
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = null;
+            try
+            {
+                response = request.GetResponse();
+                // 获得流
+                StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                html = sr.ReadToEnd();
+                response.Close();
+            }
+            catch (Exception ex)
+            {
+                // 本机没有联网
+                if (ex.GetType().ToString().Equals("System.Net.WebException"))
+                {
+                    MessageBox.Show("请检查你的计算机是否已连接上互联网。\n" + url, "提示");
+                }
+            }
+            return html;
+        }
+
+        /// <summary>
+        /// get all id and company id needed 
+        /// </summary>
+        public void getIdAndCompanyTd(string[] address,List<int> ID, List<int> ComID)
+        {
+
+
+            //网页头
+            string a = "\"msg\":\"OK\",\"code\":0,\"data\":{ \"totalCnt\":1,\"totalPage\":1,\"pageSize\":10,\"jobList\":[]}}:";
+
+
+            //遍历每个城市
+            for (int i = 0; i < address.Length; i++)
+            {
+                string url_get_1 = " https://www.nowcoder.com/recommend-intern/list?page=1&address=" + address[i];
+                string TolCatalog = GetContent(url_get_1);
+
+                //判断网页是否有具体信息
+                //目前不具有实际作用，因为目前page1必不为空
+                if (TolCatalog.Length <= a.Length)
+                {
+                    break;
+                }
+
+                //获取总页数
+                TolCatalog = TolCatalog.Substring(TolCatalog.IndexOf("totalPage") + 11);
+                int pageNum = int.Parse(TolCatalog.Substring(0, TolCatalog.IndexOf(",")));
+
+                //遍历每一页
+                for (int j = 1; j <= pageNum; j++)
+                {
+                    string url_get_2 = " https://www.nowcoder.com/recommend-intern/list?page=" + j + "&address=" + address[i];
+                    string catalog = GetContent(url_get_2);
+
+
+
+                    //正则表达式，提取id
+                    Regex re1 = new Regex("(?<=\"id\":).*?(?=,)", RegexOptions.None);
+                    //用正则表达式提取内容
+                    MatchCollection mc1 = re1.Matches(catalog);
+                    foreach (Match found in mc1)
+                    {
+                        ID.Add(int.Parse(found.Value));
+
+                    }
+
+                    //正则表达式，提取companyid
+                    Regex re2 = new Regex("(?<=\"internCompanyId\":).*?(?=,)", RegexOptions.None);
+                    //用正则表达式提取内容
+                    MatchCollection mc2 = re2.Matches(catalog);
+                    foreach (Match found in mc2)
+                    {
+                        ComID.Add(int.Parse(found.Value));
+
+                    }
+                    
+                }
+            }
+
+        }
+       
+        //防止网页中某项为空导致储存混乱
+       private string Insurance(string s)
+        {
+            if (s == "")
+            {
+                s = "----";
+            }
+            return s;
+        }
+        
+        /// <summary>
+        /// return List<practiceInfo>to store all information
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="ComID"></param>
+        /// <returns></returns>
+        public List<PracticeInfo>  getPracticeInfo(List<int> ID, List<int> ComID)
+        {
+            List<PracticeInfo> practiceInfo = new List<PracticeInfo>();
+             for(int i = 0; i < ID.Count; i++)
+            {
+                PracticeInfo tempInfo = new PracticeInfo();
+
+                string url = "https://www.nowcoder.com/recommend-intern/" + ComID[i] + "?jobId=" + ID[i];
+                string content = GetContent(url);
+                //清楚html占位符
+                content = content.Replace("&nbsp", " ");
+
+                //获取岗位名称
+                content = content.Substring(content.IndexOf("rec-job") + 14);
+                tempInfo.name = Insurance(content.Substring(0, content.IndexOf("<")));
+
+                //获取岗位职责
+                content = content.Substring(content.IndexOf("岗位职责"));
+                string dutycontent = content.Substring(0, content.IndexOf("</dl>")+5);
+                //清除换行符对正则的影响
+                dutycontent = dutycontent.Replace("\n", "");
+                //清楚制表符对输出时切分的影响
+                dutycontent = dutycontent.Replace("\t", "");
+                //正则提取>和<之间的内容
+                Regex re = new Regex("(?<=>).*?(?=<)", RegexOptions.None);
+                MatchCollection mc1 = re.Matches(dutycontent);
+                string dutycontentr = "";
+                foreach (Match found in mc1)
+                {
+                    dutycontentr+= found;
+                }
+                tempInfo.duty = Insurance(dutycontentr);
+
+                //获取岗位要求
+                content = content.Substring(content.IndexOf("岗位要求"));
+                string reqcontent = content.Substring(0, content.IndexOf("</dl>")+6);
+                //清除换行符对正则的影响
+                reqcontent = reqcontent.Replace("\n", "");
+                //清楚制表符对输出时切分的影响
+                reqcontent = reqcontent.Replace("\t", "");
+                MatchCollection mc2 = re.Matches(reqcontent);
+                string reqcontentr = "";
+                foreach (Match found in mc2)
+                {
+                    reqcontentr += found;
+                }
+                tempInfo.request = Insurance(reqcontentr);
+
+
+                //获取公司名称
+                content = content.Substring(content.IndexOf("teacher-name")+14);
+                tempInfo.compname = Insurance(content.Substring(0, content.IndexOf("<")));
+
+                //获取地址
+                content = content.Substring(content.IndexOf("com-lbs") + 9);
+                tempInfo.location = Insurance(content.Substring(0, content.IndexOf("<")));
+
+                //获取公司简介
+                content = content.Substring(content.IndexOf("com-detail")-12 );
+                string detailcontent = content.Substring(0, content.IndexOf("</p>")+4);
+                //清除换行符对正则的影响
+                detailcontent = detailcontent.Replace("\n", "");
+                //清楚制表符对输出时切分的影响
+                detailcontent = detailcontent.Replace("\t", "");
+                MatchCollection mc3 = re.Matches(detailcontent);
+                string detailcontentr = "";
+                foreach (Match found in mc3)
+                {
+                    detailcontentr += found;
+                }
+                tempInfo.compinfo = Insurance(detailcontentr);
+
+
+                //获取公司网址
+                content = content.Substring(content.IndexOf("http"));
+                tempInfo.compweb =Insurance( content.Substring(0, content.IndexOf("\"")));
+
+                practiceInfo.Add(tempInfo);
+            }
+            return practiceInfo;
+        }
+
+    }
+}
+
